d196f922e662554577ca0e14038071d9041705da
BragaMann <a82088@alunos.uminho.pt>
2020-03-20 20:19:48 +0000

    coronavirus

--- TP1/src/main.c
+++ TP1/src/main.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 1
-#define YY_END_OF_BUFFER 2
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,26 +361,31 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[6] =
+static const flex_int16_t yy_accept[59] =
     {   0,
-        0,    0,    2,    1,    0
+        0,    0,    0,    0,    4,    2,    2,    3,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    4,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    5,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    6,
+        7,    8,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    9,    1,   10,    1,   11,   12,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+       13,    1,    1,    1,   14,    1,    1,   15,   16,   17,
+       18,   19,    1,   20,   21,   22,    1,   23,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,29 +403,61 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[2] =
+static const YY_CHAR yy_meta[24] =
     {   0,
-        1
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[7] =
+static const flex_int16_t yy_base[61] =
     {   0,
-        0,    0,    2,    3,    3,    0
+       62,   61,    0,    0,   66,   69,   47,   69,   49,   60,
+        0,   47,   51,   39,   38,   51,   53,   45,   37,   38,
+       37,   39,   34,   28,   28,   39,   38,   31,   31,   23,
+       21,   38,   38,    1,   28,   32,   34,   27,   17,   16,
+       14,   15,    9,   18,   18,   24,   17,    9,   10,    9,
+       11,    6,    0,    0,    5,    0,   69,   69,    1,    0
     } ;
 
-static const flex_int16_t yy_def[7] =
+static const flex_int16_t yy_def[61] =
     {   0,
-        6,    6,    5,    5,    0,    5
+       59,   59,   60,   60,   58,   58,   58,   58,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,    0,   58,   58
     } ;
 
-static const flex_int16_t yy_nxt[5] =
+static const flex_int16_t yy_nxt[93] =
     {   0,
-        4,    5,    3,    5
+        8,    6,   11,   34,   58,   58,   58,   57,   56,   58,
+       12,   58,   58,   58,   35,   58,   58,   58,   58,   58,
+       55,   54,   53,   52,   51,   50,   49,   48,   47,   46,
+       45,   44,   43,   42,   41,   40,   39,   38,   37,   36,
+       34,   33,   32,   31,   30,   29,   28,   27,   26,   25,
+       24,   23,   22,   21,   20,   19,   18,   17,   16,   15,
+       14,   13,   11,   10,    9,   58,    7,    7,    5,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58
+
     } ;
 
-static const flex_int16_t yy_chk[5] =
+static const flex_int16_t yy_chk[93] =
     {   0,
-        6,    3,    5,    5
+       60,   59,   11,   34,    0,    0,    0,   56,   55,    0,
+       11,    0,    0,    0,   34,    0,    0,    0,    0,    0,
+       54,   53,   52,   51,   50,   49,   48,   47,   46,   45,
+       44,   43,   42,   41,   40,   39,   38,   37,   36,   35,
+       33,   32,   31,   30,   29,   28,   27,   26,   25,   24,
+       23,   22,   21,   20,   19,   18,   17,   16,   15,   14,
+       13,   12,   10,    9,    7,    5,    2,    1,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58,   58,   58,   58,   58,   58,   58,   58,   58,
+       58,   58
+
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -444,11 +481,14 @@ char *yytext;
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#line 448 "main.c"
+#include <time.h>
+#line 486 "main.c"
 /* Abreviaturas de ER */
-#line 450 "main.c"
+
+#line 489 "main.c"
 
 #define INITIAL 0
+#define OL 1
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -663,10 +703,9 @@ YY_DECL
 		}
 
 	{
-#line 14 "tp1.fl"
+#line 20 "tp1.fl"
 
-
-#line 670 "main.c"
+#line 709 "main.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -693,13 +732,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 6 )
+				if ( yy_current_state >= 59 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 3 );
+		while ( yy_base[yy_current_state] != 69 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -725,11 +764,23 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 16 "tp1.fl"
+#line 21 "tp1.fl"
+{ECHO; printf("\n");}
+	YY_BREAK
+case 2:
+/* rule 2 can match eol */
+YY_RULE_SETUP
+#line 22 "tp1.fl"
+{}
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 23 "tp1.fl"
 ECHO;
 	YY_BREAK
-#line 732 "main.c"
+#line 782 "main.c"
 case YY_STATE_EOF(INITIAL):
+case YY_STATE_EOF(OL):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1025,7 +1076,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 6 )
+			if ( yy_current_state >= 59 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1053,11 +1104,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 6 )
+		if ( yy_current_state >= 59 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 5);
+	yy_is_jam = (yy_current_state == 58);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1733,7 +1784,8 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 16 "tp1.fl"
+#line 23 "tp1.fl"
+
 
 int yywrap()
 { return(1); }
@@ -1751,6 +1803,12 @@ void help(){
 
 }
 
+void printTimestamp(){
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	printf("\"timestamp\":%d-%02d-%02d,\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+}
+
 int main(int argc, char* argv[]){ 
 
 	if(argc < 2){
@@ -1761,11 +1819,25 @@ int main(int argc, char* argv[]){
 	if(access(argv[1], F_OK) != -1){
 
 		if(access(argv[1], R_OK) != -1){
-			yyin = fopen(argv[1], "r");
+			char com[200];
+			
+			int len = strlen(argv[1]);
+
+			char nome[len+3];
+			strcpy(nome, argv[1]);
+			nome[len-5] = '\0';
+			strcat(nome, "JSON.json");
+
+			printf("%s\n", nome);
+
+			sprintf(com, "iconv -f cp1252 -t utf8 \"%s\"", argv[1]);
+			yyin = popen(com, "r");
 
 			yylex();
 	
 			fclose(yyin);
+
+			// printTimestamp();
 		}
 		else{
 			printf("Não possui permissão de leitura sobre o ficheiro fornecido!\n");
