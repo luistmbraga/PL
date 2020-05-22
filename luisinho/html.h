5a33fd261ca0476c1d507b5d2ee706e7700b4e10
Luís Braga <a82088@alunos.uminho.pt>
2020-05-22 09:35:29 +0100

    exemplo

---
+++ luisinho/html.h
@@ -0,0 +1,14 @@
+#ifndef __HTML_H__
+#define __HTML_H__
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+char* constroi_pagcreditos();
+
+void constroi_pagina();
+
+#endif
